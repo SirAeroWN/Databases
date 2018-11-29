19069ae3d5303ba4b222f301afd870aca00be081
norv <sicklyserver@gmail.com>
2018-11-29 01:41:52 -0500

    show currently checked out and reserved books

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+session_start();
 ?>
 
 <!-- Latest compiled and minified CSS -->
@@ -19,25 +19,11 @@
 
 <p>Welcome to the library catalog! Here you can...</p>
 
-<!-- <hr> -->
-
-<!-- <h4>Create an Account</h4> -->
-
-<!-- <form action="user.php" name="user" method="post"> -->
-<!--   <p>First name: <input type="text" name="firstName"></p> -->
-<!--   <p>Last name: <input type="text" name="lastName"></p> -->
-<!--   <p>Email: <input type="text" name="userEmail"></p> -->
-<!--   <p>Password: <input type="text" name="userPwd"></p> -->
-
-<!-- <input type="submit"> -->
-<!-- </form> -->
-<!-- <br> -->
-
 <hr>
 
 <h4>Search for Books</h4>
 
-    <form action="search.php" name="search" method="post">
+    <form action="search_page.php" name="search" method="post">
 
     <p>
     Please enter search term: <input type="text" name="input">
@@ -65,6 +51,44 @@
   <p><i>Don't know the ISBN of your book? Use the "Search for Books" feature above!</i></p>
 <input type="submit">
 </form>
+<?php
+    if (array_key_exists('last_reserved', $_SESSION)) {
+        $host     = "localhost";
+        $port     = 3306;
+        $socket   = "";
+        $user     = "root";
+        $password = "password";
+        $dbname   = "Library";
+
+        //create connection
+        $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+            or die ('Could not connect to the database server' . mysqli_connect_error());
+
+        if ($conn->connect_error) {
+            $aResult['error'] = 'bad connection';
+            //die("Connection failed: " . $conn->connect_error);
+
+        } else {
+            $sql = "SELECT distinct isbn_table.isbn, book_id, title, first_name, last_name, dewey, con, publisher.company_name AS company_name FROM isbn_table
+                JOIN writes ON isbn_table.isbn = writes.isbn
+                JOIN author ON writes.author_id = author.author_id
+                JOIN book ON isbn_table.isbn = book.isbn
+                JOIN sells ON isbn_table.isbn = sells.isbn
+                JOIN publishes ON isbn_table.isbn = publishes.isbn
+                JOIN publisher ON publishes.publisher_id = publisher.publisher_id
+                where book_id = {$_SESSION['last_reserved']};";
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                echo "<h3>You just reserved:</h3>";
+                $row = $result->fetch_assoc();
+                echo "<p>"."Book Title: ".$row["title"]."<br>Author: ".$row["first_name"]." ".$row["last_name"]."<br>ISBN: ".$row["isbn"]."<br>Condition: ".$row["con"]."</p>";
+            }
+        }
+    }
+?>
 <br>
 
 <hr>
@@ -80,6 +104,44 @@
 <input type="submit">
 
 </form>
+<?php
+    if (array_key_exists('last_checkedout', $_SESSION)) {
+        $host     = "localhost";
+        $port     = 3306;
+        $socket   = "";
+        $user     = "root";
+        $password = "password";
+        $dbname   = "Library";
+
+        //create connection
+        $conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+            or die ('Could not connect to the database server' . mysqli_connect_error());
+
+        if ($conn->connect_error) {
+            $aResult['error'] = 'bad connection';
+            //die("Connection failed: " . $conn->connect_error);
+
+        } else {
+            $sql = "SELECT distinct isbn_table.isbn, book_id, title, first_name, last_name, dewey, con, publisher.company_name AS company_name FROM isbn_table
+                JOIN writes ON isbn_table.isbn = writes.isbn
+                JOIN author ON writes.author_id = author.author_id
+                JOIN book ON isbn_table.isbn = book.isbn
+                JOIN sells ON isbn_table.isbn = sells.isbn
+                JOIN publishes ON isbn_table.isbn = publishes.isbn
+                JOIN publisher ON publishes.publisher_id = publisher.publisher_id
+                where book_id = {$_SESSION['last_checkedout']};";
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // output data of each row
+                echo "<h3>You just checked out:</h3>";
+                $row = $result->fetch_assoc();
+                echo "<p>"."Book Title: ".$row["title"]."<br>Author: ".$row["first_name"]." ".$row["last_name"]."<br>ISBN: ".$row["isbn"]."<br>Condition: ".$row["con"]."</p>";
+            }
+        }
+    }
+?>
 <br>
 
 <hr>
